072a2d5fe2f706c1dc61daab63e1b6588d04ff0c
frankc60 <frankc60@gmail.com>
2018-03-25 23:01:35 +0100

    renaming, working promise and with async

---
+++ app.js
@@ -0,0 +1,22 @@
+const jStrip = require('./jStrip');
+
+
+// Using Promise
+jStrip('https://www.bing.com', "$('title').html()")
+  .then((result) => {
+    console.log(`app2, bing=${  result}`);
+  })
+  .catch((e) => {
+    console.log(`Error: ${e}`);
+  });
+
+
+// Using Async/Await
+const fn = (async () => {
+  try {
+    const result = await jStrip('https://www.youtube.com', "$('title').html()");
+    await console.log(`async/await result: ${result}`);
+  } catch (err) {
+    console.log(`error ${err}`);
+  }
+})();
---
+++ jStrip.js
@@ -0,0 +1,75 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+
+class _CrawlPage {
+  constructor(url) {
+    this.url = url;
+    this.htmlparser2Options = {
+      withDomLvl1: true,
+      normalizeWhitespace: false,
+      xmlMode: true,
+      decodeEntities: true,
+    };
+    this.options = {
+      uri: 'blank',
+      transform(body) {
+        return cheerio.load(body, this.htmlparser2Options);
+      },
+    };
+  }
+
+  get(url) {
+    this.options.uri = url;
+    const start = Date.now();
+    return new Promise((resolve, reject) => {
+      // console.log(`crawling ${url}`);
+
+      rp(this.options)
+        .then(async ($) => {
+          await console.log(`done in ${(Date.now() - start)} ms`);
+
+          resolve($);
+        })
+        .catch((err) => {
+          reject((err));
+        });
+    });
+  }
+}
+
+const CrawlPage = url => new _CrawlPage(url); // so don't need a contructor new call
+
+const crawlpage = CrawlPage();
+
+
+/*
+crawlpage.get('https://www.google.com')
+  .then(($) => {
+    console.log(`via promise: ${$('title').html()}`);
+  })
+  .catch((err) => {
+    console.log(`promise Error: ${err}`);
+  });
+*/
+
+const jStrip = async (uri, jquery) => {
+  try {
+    const $ = await crawlpage.get(uri);
+    const arg = await new Function('$,jquery', `return ${jquery}`);
+    const x = await arg($, jquery);
+    // await console.log(`${uri} page crawled`);
+    // await console.log(`via await: ${x}`); // eval(jquery) - look for alternative
+    return x;
+  } catch (err) {
+    // console.log(`async Error ${err}`);
+    throw err;
+  }
+};
+
+// EXAMPLE USAGE:
+// jStrip('https://www.bbc.co.uk', "$('title').html()");
+// jStrip('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function', "$('title').html()");
+
+
+module.exports = jStrip;
