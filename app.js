ecbc70516cbac5f897979630bbcf18adb2b4bd2d
frankc60 <frankc60@gmail.com>
2018-03-26 17:00:02 +0100

    testing

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const jStrip = require('./jStrip');
-const jQuery = require("jquery");
 
 // Using Promise
 jStrip('https://www.bing.com', "$('title').html()")
@@ -14,9 +13,10 @@ jStrip('https://www.bing.com', "$('title').html()")
 // Using Async/Await
 const fn = (async () => {
   try {
-    const result = await jStrip('https://www.youtube.com', "$('title').html()");
+    const result = await jStrip('https://www.youtube.com', "$('title').html");
     await console.log(`async/await result: ${result}`);
   } catch (err) {
     console.log(`error ${err}`);
   }
 })();
+
