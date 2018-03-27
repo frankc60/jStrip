82315d0f9f0c7b4286f6437ec2d309730ca481bf
frankc60 <frankc60@gmail.com>
2018-03-27 15:28:02 +0100

    update type

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ jStrip('https://www.bing.com', "$('title').html()")
 // Using Async/Await
 const fn = (async () => {
   try {
-    const result = await jStrip('https://www.youtube.com', "$('title').html");
+    const result = await jStrip('https://www.bing.com', "$('title').html()");
     await console.log(`async/await result: ${result}`);
   } catch (err) {
     console.log(`error ${err}`);
