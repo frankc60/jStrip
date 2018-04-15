6b70c72153dae4e320f40ce4e903dc00ad7fefc8
frankc60 <frankc60@gmail.com>
2018-04-15 10:32:07 +0100

    demo

--- jsStripDemo.js
+++ jsStripDemo.js
@@ -1,17 +1,22 @@
 const jStrip = require("./jStrip");
 
-
+//******************************************************************
 let a = new jStrip();
 
-
-a.getData('https://www.google.com/').selector('title').replace(/G/,"g").marker('m2').show();
-
-a.on("m2", (d) => {
-
-console.log(d.data)
-
+a.on("m1", (d) => {
+  console.log("reply from google.com: before replace: " + d.data)
 })
 
+
+a.on("m2", (d) => {
+  console.log("reply from google.com: after replace: " + d.data)
+})
+
+a.getData('https://www.google.com/').selector('title').marker('m1').replace(/G/,"g").marker('m2').show();
+//******************************************************************
+
+
+//******************************************************************
 let jStrip4 = new jStrip();
 
 jStrip4.on("marker3", (d) => {
@@ -21,14 +26,16 @@ jStrip4.on("marker3", (d) => {
 jStrip4.getData('https://goo.gl/e234y2').selector("div#rs1")
   .marker("marker2").selector("#i_time").marker("marker3");
 jStrip4.pretty().show(); 
-
+//******************************************************************
 
 let jStrip5 = new jStrip();
 
 jStrip5.getData('hello world').replace("hello","hi").show();
 jStrip5.pretty().show(); 
+//******************************************************************
 
 const jStrip6 = new jStrip();
 
 jStrip6.getData('<b>tttt</b><u>yyyy</u>').pretty().show();
 jStrip6.removehtml().show(); 
+//******************************************************************
